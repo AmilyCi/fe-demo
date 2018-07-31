798c0f672124105a239ff0603d6297d954310be9
cx <cx@yfandeMacBook-Pro.local>
2018-07-31 14:26:43 +0800

    用原生JS实现设定目标时间倒计时功能

---
+++ count down/index.js
@@ -0,0 +1,23 @@
+var oBox =document.getElementById("box");
+oBox.innerHTML = "距离目标时间:" + getSpanTime();
+var timer = window.setInterval(function () {
+    oBox.innerHTML = "距离目标时间:" + getSpanTime();
+},1000);
+function getSpanTime(){
+    var tarTime = new Date("2018/12/20 18:00:00"),
+        nowTime = new Date(),
+        tarSpan = tarTime.getTime(),
+        nowSpan = nowTime.getTime(),
+        diffSpan = tarSpan - nowSpan,
+        hour = Math.floor(diffSpan/(1000*60*60)),
+        hourMs = hour*60*60*1000,
+        spanMs = diffSpan-hourMs,
+        minute = Math.floor(spanMs/(1000*60)),
+        minuteMs = minute*60*1000,
+        spanMs = spanMs - minuteMs,
+        second = Math.floor(spanMs/1000);
+        return zero(hour) + ":" + zero(minute) + ":" + zero(second);
+}
+function zero(value){
+   return value < 0? "0" + value : value;
+}
