7b51a56dd84501b0a5a1f0952b1ee707f7247f55
cx <cx@yfandeMacBook-Pro.local>
2018-08-02 22:01:40 +0800

    用原生JS实现页面回到顶部功能

--- Scroll to TOP/index.js
+++ Scroll to TOP/index.js
@@ -1,13 +1,15 @@
 let goLink = document.getElementById("goLink");
 
 goLink.onclick = function () {
-    let duration = 500,interval = 10,target = document.documentElement.scrollTop || document.body.scrollTop;
+    let duration = 500,
+        interval = 10,
+        target = document.documentElement.scrollTop || document.body.scrollTop;
     let step = (target/duration)*interval;
 
     let timer = setInterval(function(){
         curTarget = document.documentElement.scrollTop || document.body.scrollTop;
         curTarget -= step;
-        if(curTarget === 0){
+        if(curTarget <= 0){
             clearInterval(timer);
             return;
         }
